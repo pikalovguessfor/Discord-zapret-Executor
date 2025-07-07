b978de4469c2882bd09fbab60c9f03891f62a484
pikalovguessfor <pikalovvova1303@gmail.com>
2025-07-07 03:32:11 +0300

    v 1.5.3 still rewriting, but now everything is work.

--- Discord+zapret Exec/Main.cpp
+++ Discord+zapret Exec/Main.cpp
@@ -3,14 +3,15 @@
 * First: script execute Zapret
 * Second: script execute Discord
 */
-#include <iostream>
 #include <direct.h>
 #include <tchar.h>
 #include <Windows.h>
 
+#include "FileAction.h";
+
 using namespace std;
 
-int _CreateProcces(TCHAR Path[256]) {
+int _CreateProcces(TCHAR Path[1024]) {
 
 
 	/*
@@ -27,12 +28,19 @@ int _CreateProcces(TCHAR Path[256]) {
 	return 0;
 }
 
-const wchar_t pathToZaprARR[256] = L"_resourses/zapret_path.txt";
-const wchar_t pathToDiscArr[256] = L"_resorses/zapret_path.txt";
+wchar_t pathToZaprARR[256] = L"_resourses/zapret_path.txt";
+wchar_t pathToDiscARR[256] = L"_resourses/discord_path.txt";
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {					// point of enter
 
+	wchar_t* TextFromZaprTXT = ReadInfoFromFile(pathToZaprARR);
+	wchar_t* TextFromDiscTXT = ReadInfoFromFile(pathToDiscARR);
 
+
+
+
+	_CreateProcces(TextFromZaprTXT);
+	_CreateProcces(TextFromDiscTXT);
 
 	return 0;
 }
---
+++ Discord+zapret Exec/ReadInfoFromFile.cpp
@@ -0,0 +1,15 @@
+#include <fstream>;
+
+wchar_t* ReadInfoFromFile(wchar_t path[]) {
+
+	wchar_t* text = new wchar_t[1024];
+
+	std::wifstream ifstreamObj;
+	ifstreamObj.open(path);
+
+	if (ifstreamObj.is_open()) {
+		ifstreamObj.getline(text, 1024);
+	}
+	
+	return text;
+}

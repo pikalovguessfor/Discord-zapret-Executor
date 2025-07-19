397a283b225890d10a96a7ce3ed28b3b6632e1ab
Arto <Arto@DESKTOP-JQ3SDE4>
2025-07-19 19:41:09 +0300

    some

--- Auto-Fill_Path/DirectoryWalker.cs
+++ Auto-Fill_Path/DirectoryWalker.cs
@@ -7,18 +7,29 @@ namespace Solution
 
     public class DirectoryWalker
     {
+        public int NumberOfZapr = 1;
+
+        public string DiscordWithMask;
+        public string ZapretWithMask;
+
         public string ScopeDir;
-        public string CurrentDir;
-
-
-
-        DirectoryWalker(string ScopeDir)
+        public string CurrentDirStr;
+        public string[] CurrentDir = new string[5000];
+        
+        private string WalkDirectoryes()
         {
+            
+            
 
-
-
-
+            return " ";
         }
 
+        DirectoryWalker()
+        {
+            
+            this.DiscordWithMask = "Discord.exe";
+            this.ZapretWithMask = "general(ALT" + NumberOfZapr.ToString() + ").bat";
+            
+        }
     }
 }

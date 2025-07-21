5dac314956e55f34f3fdd04bf930d2bf719f0639
Arto <Arto@DESKTOP-JQ3SDE4>
2025-07-21 16:31:55 +0300

    делаю автозаполнитель путей

---
+++ Path Auto-filler/Pathfinder.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+
+namespace Path_Auto_filler
+{
+    internal class Pathfinder
+    {
+
+        public string FilePath;
+
+
+        private static void SearchFile(string directory, string fileName)
+        {
+            try
+            {
+                // Ищем файл в текущей директории
+                string[] files = Directory.GetFiles(directory, fileName);
+                foreach (string file in files)
+                {
+                    Console.WriteLine($"Найден файл: {file}");
+                }
+
+                // Рекурсивно ищем в поддиректориях
+                string[] subDirs = Directory.GetDirectories(directory);
+                foreach (string dir in subDirs)
+                {
+                    SearchFile(dir, fileName);
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                // Пропускаем директории, к которым нет доступа
+            }
+            catch (PathTooLongException)
+            {
+                // Пропускаем пути, которые слишком длинные
+            }
+        }
+
+
+        public Pathfinder(string filename) 
+        {
+
+            Console.WriteLine("Идет поиск файла на диске ", filename);
+
+            // Получаем список всех дисков
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+
+            foreach (DriveInfo drive in allDrives)
+            {
+                if (drive.IsReady) // Проверяем, доступен ли диск
+                {
+                    Console.WriteLine($"Поиск на диске {drive.Name}...");
+                    SearchFile(drive.RootDirectory.FullName, filename);
+                }
+            }
+
+        }
+    }
+}
---
+++ Path Auto-filler/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+
+namespace Path_Auto_filler
+{
+    public static class Solution
+    {
+
+        public static void Main(string[] args)
+        {
+
+            string DiscordPath;
+            string ZapretPath;
+
+            int SwitchCaseChooseOperator = 0;
+
+            Console.WriteLine("Какую бы версию zapret вы хотели бы использовать?");
+            Console.WriteLine("Выберите и напишите номер zapret работающий у вас");
+            Console.WriteLine("1)discord, 2)general(ALT), 3)general(ALT2), 4)general(ALT3), 5)general(ALT4), 6)general(ALT5)");
+            Console.Write(">");
+            
+            SwitchCaseChooseOperator = int.Parse(Console.ReadLine());
+
+            switch (SwitchCaseChooseOperator)
+            {
+
+                case 1:
+                    
+
+
+
+
+                    break;
+                case 2:
+
+
+
+
+                    break;
+                case 3:
+
+
+
+
+                    break;
+                case 4:
+
+
+
+
+                    break;
+                case 5:
+
+
+
+
+                    break;
+                case 6:
+
+
+
+
+                    break;
+                default:
+                    Console.WriteLine("Неправильный ввод, допустимы значения 1-6");
+
+                    break;
+            }
+        }
+
+    }
+}
